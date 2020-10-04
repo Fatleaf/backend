26e8326e9db72d97679a57aa9c38cbb1d8c66451
Fatleaf <cycfree1988@gmail.com>
2020-10-05 00:05:12 +0800

    update
    名稱方式的更改，才不會搞混

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -37,14 +37,14 @@
                 {{-- {{$news_list}} --}}
 
                 {{-- foreach迴圈執行套版樣式，new_list變數來自controller，{{}}不太聊解 --}}
-                @foreach ($news_list as $news)
+                @foreach ($news_list as $value)
                 <div class="col-md-4">
                     <div class="news_list">
-                    <h3>{{$news->title}}</h3>
-                        <h4>{{$news->sub_title}}</h4>
-                        <img width="100%" src="{{$news->img_url}}" alt="圖片建議尺寸: 1000 x 567">
-                        <p class="news_content">{{$news->content}}</p>
-                    <a class="btn btn-success" href="/news_info/{{$news->id}}" role="button">點擊查看更多 &raquo;</a>
+                    <h3>{{$value->title}}</h3>
+                        <h4>{{$value->sub_title}}</h4>
+                        <img width="100%" src="{{$value->img_url}}" alt="圖片建議尺寸: 1000 x 567">
+                        <p class="news_content">{{$value->content}}</p>
+                    <a class="btn btn-success" href="/news_info/{{$value->id}}" role="button">點擊查看更多 &raquo;</a>
                     </div>
                 </div>
                 @endforeach
--- resources/views/front/news.blade.php
+++ resources/views/front/news.blade.php
@@ -12,14 +12,14 @@
             <h2 class="news_title">最新消息</h2>
 
             <div class="row news_lists">
-                @foreach ($news_list as $news)
+                @foreach ($news_list as $value)
                 <div class="col-md-4">
                     <div class="news_list">
-                        <h3>{{$news->title}}</h3>
-                        <h4>{{$news->sub_title}}</h4>
-                        <img width="100%" src="{{$news->img_url}}" alt="圖片建議尺寸: 1000 x 567">
-                        <p class="news_content">{{$news->content}}</p>
-                        <a class="btn btn-success" href="/news_info/{{$news->id}}" role="button">點擊查看更多 &raquo;</a>
+                        <h3>{{$value->title}}</h3>
+                        <h4>{{$value->sub_title}}</h4>
+                        <img width="100%" src="{{$value->img_url}}" alt="圖片建議尺寸: 1000 x 567">
+                        <p class="news_content">{{$value->content}}</p>
+                        <a class="btn btn-success" href="/news_info/{{$value->id}}" role="button">點擊查看更多 &raquo;</a>
                     </div>
                 </div>
                 @endforeach
